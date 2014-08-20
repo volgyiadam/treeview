d09fc17b838d88f545e1446d8d92a50d70c3c2f0
volgyiadam <volgyiadam@gmail.com>
2014-08-20 19:55:41 +0200

    design

---
+++ design/Controllers/DesignController.cs
@@ -0,0 +1,56 @@
+﻿using design.Models;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+
+namespace design.Controllers
+{
+    public class DesignController : Controller
+    {
+        //
+        // GET: /Design/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult FileSystemInfos(string root)
+        {
+            DirectoryInfo rootDirectory = null;
+            if (root == null || root=="source")
+                rootDirectory = new DirectoryInfo(@"C:\dev\");
+            else
+            {
+                rootDirectory = new DirectoryInfo(root);
+
+            }
+            var directoryChildren = from child in rootDirectory.GetFileSystemInfos()
+                                    orderby child is DirectoryInfo descending
+                                    select child;
+
+            List<TreeViewNode> nodes = new List<TreeViewNode>();
+            foreach (FileSystemInfo directoryChild in directoryChildren)
+            {
+                bool isDirectory = directoryChild is DirectoryInfo;
+                nodes.Add(new TreeViewNode()
+                {
+                    id = directoryChild.FullName,
+                    text = directoryChild.Name,
+                    classes = isDirectory ? "folder" : "file",
+                    hasChildren = isDirectory
+                    
+                    
+                });
+            }
+            return Json(nodes.ToArray());
+            //return new JsonResult() { Data = nodes.ToArray(), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            
+        }
+
+    }
+}
---
+++ design/Models/TreeViewNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace design.Models
+{
+    public class TreeViewNode
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public bool expanded { get; set; }
+        public string classes { get; set; }
+        public bool hasChildren { get; set; }
+        public TreeViewNode[] children { get; set; }
+    }
+}
